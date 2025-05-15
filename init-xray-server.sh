016a9241badad8d46035fc1d4621880c29d29628
unknown-7171 <unknown-7171@email.hid>
2025-05-15 16:57:18 +0400

    add shortIds

--- init-xray-server.sh
+++ init-xray-server.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# Generate X25519 keys
 _x25519=$(xray x25519)
 PRIVATE_KEY=$(echo "$_x25519" | awk -F': ' '/Private key/{print $2}')
 PUBLIC_KEY=$(echo "$_x25519" | awk -F': ' '/Public key/{print $2}')
-
 CLIENT_UUID=$(uuidgen)
+SHORT_ID=$(openssl rand -hex 8)
 CONFIG_FILE="/app/xconfig.json"
 SNI=$(jq -r '.inbounds[0].streamSettings.realitySettings.serverNames[0]' "$CONFIG_FILE")
 FINGERPRINT=$(jq -r '.inbounds[0].streamSettings.realitySettings.fingerprint' "$CONFIG_FILE")
@@ -16,9 +15,12 @@ NETWORK=$(jq -r '.inbounds[0].streamSettings.network' "$CONFIG_FILE")
 
 jq --arg uuid "$CLIENT_UUID" \
    --arg private_key "$PRIVATE_KEY" \
+   --arg short_id "$SHORT_ID" \
    '.inbounds[0].settings.clients[0].id = $uuid |
-    .inbounds[0].streamSettings.realitySettings.privateKey = $private_key' \
+    .inbounds[0].streamSettings.realitySettings.privateKey = $private_key |
+    .inbounds[0].streamSettings.realitySettings.shortIds = [$short_id]' \
    "$CONFIG_FILE" > /tmp/xconfig_tmp.json && mv /tmp/xconfig_tmp.json "$CONFIG_FILE"
 
-CLIENT_LINK="vless://${CLIENT_UUID}@${SERVER_ADDRESS}:443?security=${SECURITY}&type=${NETWORK}&sni=${SNI}&fp=${FINGERPRINT}&pbk=${PUBLIC_KEY}&flow=${FLOW}&sid=#xray-server"
+CLIENT_LINK="vless://${CLIENT_UUID}@${SERVER_ADDRESS}:443?security=${SECURITY}&type=${NETWORK}&sni=${SNI}&fp=${FINGERPRINT}&pbk=${PUBLIC_KEY}&flow=${FLOW}&sid=${SHORT_ID}#xray-server"
+
 echo "$CLIENT_LINK" | tee /app/client-vless-url.txt
